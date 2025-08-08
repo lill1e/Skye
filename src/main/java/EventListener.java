b20c35ea1400b537693356cb0fb0f046d90de426
lillie <onlinechronically@protonmail.com>
2025-08-08 05:15:27 -0400

    add: event for players quitting

--- src/main/java/EventListener.java
+++ src/main/java/EventListener.java
@@ -3,7 +3,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class EventListener implements Listener {
@@ -21,6 +23,18 @@ public class EventListener implements Listener {
                 player.kickPlayer("There was a problem fetching your account");
             } catch (Exception ignored) {
             }
+        } catch (Exception ignored) {
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        try (ServerPlayer serverPlayer = Plugin.playerManager.get(player)) {
+            PreparedStatement stmt = ServerPlayer.connection.prepareStatement("UPDATE USERS SET balance = ? WHERE id = ?");
+            stmt.setInt(1, serverPlayer.balance);
+            stmt.setObject(2, serverPlayer.id);
+            serverPlayer.remove();
         } catch (Exception ignored) {
         }
     }
